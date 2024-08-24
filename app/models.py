753487b5116f62f40ed76e313eea78db99a47553
website-krysta <website.krysta@gmail.com>
2024-08-24 20:23:39 +0530

    change model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Slider(models.Model):
-    image = models.ImageField(upload_to='slider_images/')
+    image = models.ImageField(upload_to='media/slider/')
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
     link = models.URLField(max_length=200, blank=True, null=True)
