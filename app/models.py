ac908a08c3b4993b1c37bd0475067b990f6e8bff
website-krysta <website.krysta@gmail.com>
2024-08-25 17:11:33 +0530

    fixed issues

--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Slider(models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='slider/')
+    title = models.CharField(max_length=100,blank=True, null=True)
+    subtitle = models.CharField(max_length=100,blank=True, null=True)
     link = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
